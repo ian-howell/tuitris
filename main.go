d3fce5102c6e92877e39706d588056715b6dce6f
Ian Howell <ian.howell0@gmail.com>
2024-06-21 21:54:32 -0500

    Finish the transition table

--- main.go
+++ main.go
@@ -11,11 +11,25 @@ import (
 
 func main() {
 
+	splashMenu, err := ring.New(
+		Choice{
+			Name:       "Menu",
+			Cmd:        MenuCmd,
+			NextScreen: MenuScreen,
+		},
+	)
+	checkError(err)
+
 	menuMenu, err := ring.New(
 		Choice{
 			Name:       "Init",
 			Cmd:        InitCmd,
 			NextScreen: InitScreen,
+		},
+		Choice{
+			Name:       "Options",
+			Cmd:        OptionsCmd,
+			NextScreen: OptionsScreen,
 		},
 		Choice{
 			Name:       "Exit",
@@ -25,10 +39,111 @@ func main() {
 	)
 	checkError(err)
 
+	optionsMenu, err := ring.New(
+		Choice{
+			Name:       "Menu",
+			Cmd:        MenuCmd,
+			NextScreen: MenuScreen,
+		},
+	)
+	checkError(err)
+
+	initScreen, err := ring.New(
+		Choice{
+			Name:       "Play",
+			Cmd:        PlayCmd,
+			NextScreen: PlayScreen,
+		},
+	)
+	checkError(err)
+
+	playScreen, err := ring.New(
+		Choice{
+			Name:       "Pause",
+			Cmd:        PauseCmd,
+			NextScreen: PauseScreen,
+		},
+		Choice{
+			Name:       "Win",
+			Cmd:        WinCmd,
+			NextScreen: WinScreen,
+		},
+		Choice{
+			Name:       "Lose",
+			Cmd:        LoseCmd,
+			NextScreen: LoseScreen,
+		},
+	)
+	checkError(err)
+
+	pauseScreen, err := ring.New(
+		Choice{
+			Name:       "Init",
+			Cmd:        InitCmd,
+			NextScreen: InitScreen,
+		},
+		Choice{
+			Name:       "Menu",
+			Cmd:        MenuCmd,
+			NextScreen: MenuScreen,
+		},
+		Choice{
+			Name:       "Exit",
+			Cmd:        ExitCmd,
+			NextScreen: ErrorScreen,
+		},
+	)
+	checkError(err)
+
+	winScreen, err := ring.New(
+		Choice{
+			Name:       "Init",
+			Cmd:        InitCmd,
+			NextScreen: InitScreen,
+		},
+		Choice{
+			Name:       "Menu",
+			Cmd:        MenuCmd,
+			NextScreen: MenuScreen,
+		},
+		Choice{
+			Name:       "Exit",
+			Cmd:        ExitCmd,
+			NextScreen: ErrorScreen,
+		},
+	)
+	checkError(err)
+
+	loseScreen, err := ring.New(
+		Choice{
+			Name:       "Init",
+			Cmd:        InitCmd,
+			NextScreen: InitScreen,
+		},
+		Choice{
+			Name:       "Menu",
+			Cmd:        MenuCmd,
+			NextScreen: MenuScreen,
+		},
+		Choice{
+			Name:       "Exit",
+			Cmd:        ExitCmd,
+			NextScreen: ErrorScreen,
+		},
+	)
+	checkError(err)
+
 	initialModel := Model{
-		CurrentScreen: MenuScreen,
+		CurrentScreen: SplashScreen,
 		Menus: map[Screen]ring.Ring[Choice]{
-			MenuScreen: menuMenu,
+			SplashScreen:  splashMenu,
+			MenuScreen:    menuMenu,
+			OptionsScreen: optionsMenu,
+			InitScreen:    initScreen,
+			PlayScreen:    playScreen,
+			PauseScreen:   pauseScreen,
+			WinScreen:     winScreen,
+			LoseScreen:    loseScreen,
 		},
 	}
 
--- screenmanager.go
+++ screenmanager.go
@@ -41,3 +41,35 @@ var _ tea.Msg = InitCmd
 func InitCmd() tea.Msg {
 	return InitScreen
 }
+
+var _ tea.Msg = PlayCmd
+
+func PlayCmd() tea.Msg {
+	return PlayScreen
+}
+
+var _ tea.Msg = OptionsCmd
+
+func OptionsCmd() tea.Msg {
+	return OptionsScreen
+}
+
+var _ tea.Msg = PauseCmd
+
+func PauseCmd() tea.Msg {
+	return PauseScreen
+}
+
+var _ tea.Msg = WinCmd
+
+func WinCmd() tea.Msg {
+	return WinScreen
+}
+
+var _ tea.Msg = LoseCmd
+
+func LoseCmd() tea.Msg {
+	return LoseScreen
+}
+
+var ExitCmd = tea.Quit
